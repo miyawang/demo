e214efd9120ae153540ad151e8707088f5ae2666
miyawang <miyawang1019@gmail.com>
2018-05-17 19:33:55 +0800

    5.17

--- 01-add.php
+++ 01-add.php
@@ -1,61 +1,108 @@
 <?php 
-    
-    //获取前端提交的数据 和 图片 
-    //把数据存储到数据库(data.txt)中
-
-    //注意点：
-    //一个人信息用一维数组存储
-    //一个班的信息用二维数组存储
-    //添加学生信息 ， 就是向二维数组中追加一个一维数组
-    //删除学生信息 ， 就是从二维数组中删除一个一维数组
-    //数组就是变量 ，页面关闭数据销毁， 要把数据存到data.txt中， 为了持久化
-
     // echo '<pre>';
     // print_r($_POST);
     // echo '</pre>';
-    
+
     // echo '<pre>';
     // print_r($_FILES);
     // echo '</pre>';
 
+    // [username] => baby
+    // [nickname] => 铔嬭泲
+    // [age] => 22
+    // [tel] => 12312312323
+    // [sex] => m
+    // [class] => 4
+    // 引入操作数据工具函数
+    // include_once './fn.php';
 
-    //1-用一维数组来存储这个人的信息
-    $info = $_POST;
-    //2-将文件的保存到upload文件夹中，把文件在服务器中地址，存放到数据库中
-    $photo = $_FILES['photo'];
-    if ($photo['error'] === 0) {
-        $ftmp = $photo['tmp_name']; //临时文件路径
-        $ext = strrchr( $photo['name'], '.' ); //截取后缀名
-        $newName = './upload/'. time() . rand(1000, 9999) . $ext; //新文件路径
-        //转移文件
-        move_uploaded_file($ftmp, $newName);
-        //把新文件路径添加到info中
-        $info['photo'] = $newName;
-    }
+    // //1-接收前端传递数
+    // $username = $_POST['username'];
+    // $nickname = $_POST['nickname'];
+    // $age = $_POST['age'];
+    // $tel = $_POST['tel'];
+    // $sex = $_POST['sex'];
+    // $class = $_POST['class'];
+    // $photo = '';
 
-    // echo '<pre>';
-    // print_r($info);
-    // echo '</pre>';
+    // //2-保存上传图像
+    // $file = $_FILES['photo'];
+    // //如果图片上传成功则保存图片
+    // if ($file['error'] === 0) {
+    //     $ftmp = $file['tmp_name'];//临时文件路径
+    //     $ext = strrchr($file['name'], '.');
+    //     $newName = './uploads/'. time() .rand(1000, 9999) . $ext;
+    //     //转移
+    //     move_uploaded_file($ftmp, $newName);
+    //     $photo  = $newName; //保存图片在服务器中存储地址
+    // }
+    // // 13277948971 
+    // //3-把添加学生信息和图片地址 存放数据库中
+    // //准备添加数据sql语句
+    // $sql = "insert into stu (name, nickname, age, tel, sex, photo , classid) 
+    //         values ('$username', '$nickname', $age, '$tel', '$sex', '$photo', $class)";
 
-    //把个人信息添加到数据库中
-    //info数组添加到data.txt中数组中
-    //1-先把data.txt中空数组取出(json是字符串)
-    $str = file_get_contents('data.txt');
-   
-    //2-把json字符串转成数组
-    $arr = json_decode($str, true);
+    // echo $sql;
 
-    //3-把info添加到数组中
-    $arr[] = $info;
+    // my_exec($sql); //执行sql语句
 
-    //4-在将数组转成json字符串
-    $str = json_encode($arr);
- 
-    //5-把json字符串存储data.txt中
-    file_put_contents('data.txt', $str);
+    // //跳转到列表页
+    // header('location:02-list.php');
+
+
+    // include_once './fn.php'; // 引入工具函数
+    // $username = $_POST['username'];
+    // $nickname = $_POST['nikename'];
+    // $age = $_POST['age'];
+    // $tel = $_POST['tel'];
+    // $sex = $_POST['sex'];
+    // $class = $_POST['class'];
+    // $photo = '';
+    // //$_GET $_POST $_FILES是PHP系统提供的超全局变量，是一个数组，里面存放了表单通过get方式提交的数据。
+    // $file = $_FILES['photo']; //二维数组
+
+    // if($file['error'] === 0){
+    //     $ftmp = $file['tmp_name'];
+    //     $ext = strrchr($file['name'],'.');
+    //     $newName = './uploads/' .time().rand(1000,9999).$ext;
+    //     move_uploaded_file($ftmp,$newName);
+    //     // move_uploaded_file($path, $newPath);可以保存临时图片
+    //     $photo = $newName;
+    // }
+    // $sql = "insert into stu (name,nickname,age,tel,sex,photo,classid)
+    //         values ('$username','$nickname',$age,'$tel','$sex','$photo',class)";
+    // echo $sql;
+    // my_exec($sql); //执行
+    // header('location:02-list.php'); //跳转
+    
     
 
-    //跳转到列表页
-    //header('location:02-list.php');
+    include_once './fn.php';
+    $username = $_POST['username'];
+    $nickname = $_POST['nikename'];
+    $age = $_POST['age'];
+    $tel = $_POST['tel'];
+    $sex = $_POST['sex'];
+    $class = $_POST['class'];
+    $photo = '';
+
+    $file = $_FILES['photo'];
+    if($file['error'] ===0){
+        $ftmp = $file['tmp_name'];
+        $ext = strrchr($file['name'],'.');//从右往左
+        $newName = './uploads/'.time().rand(1000,9999).$ext;
+        move_uploaded_file($ftmp,$newName);
+        $photo = $newName;
+    }
+    $sql = "insert into stu (name,nickname,age,tel,sex,photo,classid)
+    values('$username','$nickname','$age','$tel','$sex','$photo','$class')";
+    echo $sql;
+    my_exec($sql);
+    header('location:02-list.php');
+
+
+
+
+    
 
 ?>
